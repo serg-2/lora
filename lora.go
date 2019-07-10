04f6a3fec66ed7cf3cde4ff701f20ee19e105d94
serg-2 <serg____@mail.ru>
2019-07-10 18:58:39 +0000

    Changed to Configuration file

--- lora.go
+++ lora.go
@@ -4,6 +4,7 @@ import "fmt"
 import "os"
 import "time"
 import "strconv"
+import "encoding/json"
 
 import "github.com/serg-2/libs-go/cryptolib"
 import "github.com/serg-2/libs-go/loralib"
@@ -16,6 +17,12 @@ var message_source string
 var key []byte
 
 var send_signal <-chan time.Time
+
+type Configuration struct {
+	Baud_rate   int
+	Serial_port string
+	Key         string
+}
 
 func main_func() {
 	loralib.InitiateRPIO()
@@ -48,6 +55,18 @@ func main_func() {
 }
 
 func main() {
+	var conf Configuration
+	//filename is the path to the json config file
+	file, err := os.Open("configuration.json")
+	if err != nil {
+		return
+	}
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&conf)
+	if err != nil {
+		return
+	}
+
 	if len(os.Args[1:]) != 2 {
 		fmt.Printf("Usage: %v <time to send> <string to send> \n", os.Args[0])
 		os.Exit(0)
@@ -56,15 +75,13 @@ func main() {
 	message_source = os.Args[2]
 	time_from_arg, _ := strconv.Atoi(os.Args[1])
 
-	key = []byte("Mega secret key SUper 123OGON !!") // 32 bytes \ 256 bit
+	key = []byte(conf.Key) // 32 bytes \ 256 bit
 	send_message, _ = cryptolib.Encrypt(key, []byte(message_source))
 
 	send_signal = time.Tick(time.Duration(time_from_arg) * time.Second)
 
-//Serial Part	
-	baud_rate := 9600
-	serial_port := "/dev/ttyS0"
-	x, y, status := seriallib.GetPosition("GGA", serial_port, baud_rate, true)
+	//Serial Part
+	x, y, status := seriallib.GetPosition("GGA", conf.Serial_port, conf.Baud_rate, true)
 	if status {
 		fmt.Printf("%v --- %v\n", x, y)
 	}
