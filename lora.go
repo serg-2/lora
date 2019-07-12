ae52efaa959dfdb729dfa620cb003a39040cab43
serg-2 <serg____@mail.ru>
2019-07-12 16:04:15 +0000

    WIP: distance

--- lora.go
+++ lora.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 import "os"
 import "time"
-import "strconv"
 import "encoding/json"
+import "strings"
 
 import "github.com/serg-2/libs-go/cryptolib"
 import "github.com/serg-2/libs-go/loralib"
@@ -15,13 +15,31 @@ var receivedbytes byte
 var send_message []byte
 var message_source string
 var key []byte
+var x,y float64
+var status bool
 
-var send_signal <-chan time.Time
+var conf Configuration
+
+var send_signal,update_timer <-chan time.Time
 
 type Configuration struct {
 	Baud_rate   int
 	Serial_port string
 	Key         string
+	Sending_timer int
+        Update_coordinate_timer int
+	Base_coordinates string
+}
+
+func update_coordinate() {
+	for {
+		<-update_timer
+		for status !=true {
+			x, y, status = seriallib.GetPosition("GGA", conf.Serial_port, conf.Baud_rate, true)
+		}
+		status = false
+		//fmt.Println ("Coordinate updated")
+	}
 }
 
 func main_func() {
@@ -36,6 +54,8 @@ func main_func() {
 	for {
 		select {
 		case <-send_signal:
+			message_source = fmt.Sprintf("%09.6f,%010.6f",x,y)
+			send_message, _ = cryptolib.Encrypt(key, []byte(message_source))
 			loralib.Send(send_message)
 			fmt.Printf("Send: %s\n", message_source)
 
@@ -48,6 +68,9 @@ func main_func() {
 			if status {
 				decrypted_message, _ := cryptolib.Decrypt(key, received_message)
 				fmt.Printf("Payload: %s\n", string(decrypted_message))
+				if len(strings.Split(string(decrypted_message),",")) != 2 {
+					fmt.Println("err")
+				}
 			}
 			time.Sleep(500 * time.Millisecond)
 		}
@@ -55,36 +78,29 @@ func main_func() {
 }
 
 func main() {
-	var conf Configuration
-	//filename is the path to the json config file
+
 	file, err := os.Open("configuration.json")
 	if err != nil {
-		return
+		fmt.Printf("Can't open configuration file: configuration.json\n")
+		os.Exit(0)
 	}
 	decoder := json.NewDecoder(file)
 	err = decoder.Decode(&conf)
 	if err != nil {
-		return
+		fmt.Printf("Invalid JSON in configuration file.\n")
+                os.Exit(0)
 	}
 
-	if len(os.Args[1:]) != 2 {
-		fmt.Printf("Usage: %v <time to send> <string to send> \n", os.Args[0])
+	if len(os.Args[1:]) != 0 {
+		fmt.Printf("Usage: %v\n", os.Args[0])
 		os.Exit(0)
 	}
 
-	message_source = os.Args[2]
-	time_from_arg, _ := strconv.Atoi(os.Args[1])
-
 	key = []byte(conf.Key) // 32 bytes \ 256 bit
-	send_message, _ = cryptolib.Encrypt(key, []byte(message_source))
+	send_signal = time.Tick(time.Duration(conf.Sending_timer) * time.Second)
+	update_timer = time.Tick(time.Duration(conf.Update_coordinate_timer) * time.Second)
 
-	send_signal = time.Tick(time.Duration(time_from_arg) * time.Second)
-
-	//Serial Part
-	x, y, status := seriallib.GetPosition("GGA", conf.Serial_port, conf.Baud_rate, true)
-	if status {
-		fmt.Printf("%v --- %v\n", x, y)
-	}
+	go update_coordinate()
 
 	main_func()
 
